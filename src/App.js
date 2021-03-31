4b6f6a9bc012968fff3f498a0e937f35ffbef82b
Yasmen-K <kodjabashevayasemin@gmail.com>
2021-03-31 21:48:16 +0300

    Owner Information Component Edited

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
 
       <Header/>
     <AppRoutes/>
+    
     </div>
       
       
--- src/components/Account/OwnerInformation/OwnerInformation.js
+++ src/components/Account/OwnerInformation/OwnerInformation.js
@@ -3,13 +3,30 @@ import styles from './OwnerInformation.module.css'
 
 const OwnerInformation = () => {
     return ( 
-        <div>
-        <form className={styles['main']}>
-            <label htmlFor="owner-name"></label>
+        
+      
+
+        <form className={styles['container']}>
+            <div className={styles['main']}>
+
+            <label htmlFor="owner-name">Enter Owner Name</label>
             <input type="text" id="owner-name" name="owner-name"/>
+
+            <label htmlFor="owner-phone">Enter Owner Phone</label>
+            <input type="number" name="owner-phone"/>
+
+            <button>Submit</button>
+            </div>
+        
+
+        <img src="OwnerInfromation.jpg" alt=""/>
+        
         </form>
 
-        </div>
+
+        
+
+        
      );
 }
  
